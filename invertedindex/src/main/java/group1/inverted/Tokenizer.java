8a27f5cb11854e01c75c0f92ca8f06e2af92d0af
Brian Clements <brian@clementscode.com>
2011-12-03 20:32:37 -0600

    switched tokenizer from sax to simple indexer

--- invertedindex/src/main/java/group1/inverted/Tokenizer.java
+++ invertedindex/src/main/java/group1/inverted/Tokenizer.java
@@ -1,20 +1,13 @@
 package group1.inverted;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.hadoop.io.Text;
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 public class Tokenizer implements Iterable<String> {
 
@@ -22,68 +15,126 @@ public class Tokenizer implements Iterable<String> {
 
 	protected List<String> tokens = new ArrayList<String>(50);
 
+	protected static final String ID_TAG = "DOCID";
+
+	protected static final String TITLE_TAG = "TITLE";
+
+	protected static final String TEXT_TAG = "TEXT";
+
+	protected String data;
+
+	protected int curr;
+
 	protected static SAXParserFactory saxFact = SAXParserFactory.newInstance();
 
-	protected DefaultHandler handler = new DefaultHandler() {
+	protected enum TYPE {
+		DOCID, TEXT, PASS
+	};
 
-		protected boolean inDocid = false;
+	protected static class TextRun {
+		public TYPE type = TYPE.TEXT;
+		public String value;
 
-		protected boolean inText = false;
+		public TextRun(String value, TYPE type) {
+			this.value = value;
+			this.type = type;
+		}
+	}
 
-		protected static final String ID_TAG = "DOCID";
+	protected static class TextBounds {
+		public boolean done = false;
+		public int start;
+		public int end;
+	}
 
-		protected static final String TITLE_TAG = "TITLE";
+	public Tokenizer(Text t) throws Exception {
+		parse(t);
+	}
 
-		protected static final String TEXT_TAG = "TEXT";
+	protected Tokenizer() {
+		// unit tests
+	}
 
-		@Override
-		public void characters(char[] ch, int start, int length)
-				throws SAXException {
-			if (inText) {
-				String str = new String(ch, start, length);
-				StringTokenizer toker = new StringTokenizer(str, " \t\n.,");
+	protected TextRun getNext() {
+		TextBounds tag = findTag(curr);
+		if (tag.done) {
+			return null;
+		}
+		TextRun rval;
+		String tagName = getBeween(tag);
+		if (tagName.equalsIgnoreCase(ID_TAG)) {
+			TextBounds close = findClose(tag);
+			String id = getBeween(tag, close).trim();
+			rval = new TextRun(id, TYPE.DOCID);
+			curr = close.end;
+		} else if (tagName.equalsIgnoreCase(TEXT_TAG)
+				|| tagName.equalsIgnoreCase(TITLE_TAG)) {
+			TextBounds close = findClose(tag);
+			String str = getBeween(tag, close);
+			rval = new TextRun(str, TYPE.TEXT);
+			curr = close.end;
+		} else {
+			rval = new TextRun(null, TYPE.PASS);
+			curr = tag.end;
+		}
+		return rval;
+	}
+
+	protected TextBounds findClose(TextBounds open) {
+		TextBounds close = findTag(open.end);
+		if (close.done) {
+			close.start = data.length();
+			close.end = close.start;
+		}
+		return close;
+	}
+
+	protected TextBounds findTag(int i) {
+		TextBounds rval = new TextBounds();
+		rval.start = data.indexOf('<', i);
+		rval.done = rval.start == -1;
+		if (!rval.done) {
+			rval.end = data.indexOf('>', rval.start);
+			if (rval.end == -1) {
+				rval.end = data.length() - 1;
+			}
+		}
+		return rval;
+	}
+
+	protected String getBeween(TextBounds tb) {
+		return data.substring(tb.start + 1, tb.end);
+	}
+
+	protected String getBeween(TextBounds open, TextBounds close) {
+		return data.substring(open.end + 1, close.start);
+	}
+
+	protected void parse(Text t) {
+		data = new String(t.getBytes()).trim();
+
+		TextRun run = getNext();
+		while (run != null) {
+			switch (run.type) {
+			case DOCID:
+				this.docId = run.value;
+				break;
+			case TEXT:
+				StringTokenizer toker = new StringTokenizer(run.value,
+						" \t\n.,");
 				while (toker.hasMoreTokens()) {
 					String next = toker.nextToken();
 					if (next.length() >= 5) {
 						tokens.add(next);
 					}
 				}
-				inText = false;
-			} else if (inDocid) {
-				String str = new String(ch, start, length);
-				docId = str;
-				inDocid = false;
+				break;
+			case PASS:
+			default:
+				// do nothing
 			}
+			run = getNext();
 		}
-
-		@Override
-		public void startElement(String uri, String localName, String qName,
-				Attributes attributes) throws SAXException {
-			if (qName.equalsIgnoreCase(ID_TAG)) {
-				this.inDocid = true;
-				this.inText = false;
-			} else if (qName.equalsIgnoreCase(TITLE_TAG)
-					|| qName.equalsIgnoreCase(TEXT_TAG)) {
-				this.inDocid = false;
-				this.inText = true;
-			} else {
-				this.inDocid = false;
-				this.inText = false;
-			}
-		}
-
-	};
-
-	public Tokenizer(Text t) throws Exception {
-		parse(t);
-	}
-
-	protected void parse(Text t) throws ParserConfigurationException,
-			SAXException, IOException {
-		SAXParser parser = saxFact.newSAXParser();
-		String trimmed = new String(t.getBytes()).trim();
-		ByteArrayInputStream bin = new ByteArrayInputStream(trimmed.getBytes());
-		parser.parse(bin, handler);
 	}
 
 	public String getDocId() {
--- invertedindex/src/test/java/group1/inverted/TokenizerTest.java
+++ invertedindex/src/test/java/group1/inverted/TokenizerTest.java
@@ -1,5 +1,7 @@
 package group1.inverted;
 
+import group1.inverted.Tokenizer.TextBounds;
+
 import java.util.Iterator;
 
 import junit.framework.Assert;
@@ -19,10 +21,23 @@ public class TokenizerTest {
 			+ "<SUBJECT> BC-MIDEAST-AID </SUBJECT>\n" + "<AUTHOR></AUTHOR>\n"
 			+ "<TEXT>the cake is a lie  flabber newline</TEXT></DOC>";
 
+	protected String badXml = "<DOC>\n"
+			+ "<DOCID>REU001-0013.940701<WhoKnows>\n"
+			+ "<TDTID>TDT000002</TDTID>\n"
+			+ "<SOURCE>REUTERS</SOURCE>\n"
+			+ "<DATE>07/01/94 00:57</DATE>\n"
+			+ "<TITLE> PALESTINIANS TO RECEIVE AID TO POLICE GAZA, JERICHO</TITLE>\n"
+			+ "<HEADLINE> PALESTINIANS TO RECEIVE AID TO POLICE GAZA, JERICHO</HEADLINE>\n"
+			+ "<SUBJECT> BC-MIDEAST-AID </SUBJECT>\n" + "<AUTHOR></AUTHOR>\n"
+			+ "<TEXT>the cake is a lie  flabber newline";
+
 	@Test
 	public void simpleTest() throws Exception {
-		System.out.println(simpleXml);
-		Text text = new Text(simpleXml);
+		assertXml(simpleXml);
+	}
+
+	public void assertXml(String xml) throws Exception {
+		Text text = new Text(xml);
 		Tokenizer tokenizer = new Tokenizer(text);
 		String docId = tokenizer.getDocId();
 		Assert.assertEquals("REU001-0013.940701", docId);
@@ -36,4 +51,26 @@ public class TokenizerTest {
 		Assert.assertEquals(false, i.hasNext());
 	}
 
+	@Test
+	public void tagTest() {
+		Tokenizer toker = new Tokenizer();
+		String xml = "<foo>bar</foo><next>one</next>";
+		toker.data = xml;
+		TextBounds open = toker.findTag(0);
+		TextBounds close = toker.findTag(open.end);
+		Assert.assertEquals("foo", toker.getBeween(open));
+		Assert.assertEquals("bar", toker.getBeween(open, close));
+		open = toker.findTag(close.end);
+		close = toker.findTag(open.end);
+		Assert.assertEquals("next", toker.getBeween(open));
+		Assert.assertEquals("one", toker.getBeween(open, close));
+		open = toker.findTag(close.end);
+		Assert.assertTrue(open.done);
+
+	}
+
+	@Test
+	public void malformTest() throws Exception {
+		assertXml(badXml);
+	}
 }
