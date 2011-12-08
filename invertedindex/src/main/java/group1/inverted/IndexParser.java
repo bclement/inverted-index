482b292f3da408991d9470b4cd3329cb1d2eab46
krboswell <krboswell@gmail.com>
2011-12-08 13:47:29 -0600

    indexparser unittest

--- invertedindex/src/main/java/group1/inverted/IndexParser.java
+++ invertedindex/src/main/java/group1/inverted/IndexParser.java
@@ -1,6 +1,5 @@
 package group1.inverted;
 
-import java.io.IOException;
 
 public class IndexParser {
 	
@@ -8,16 +7,22 @@ public class IndexParser {
 		public String word = null;
 		public Integer count = null;
 		public String docid = null;
+		
+		@Override
+		public String toString()
+		{
+			return word + ":" + count + ":" + docid;
+		}
 	}
 
-	public static Index parseLine(final String line) throws IOException {
+	public static Index parseLine(final String line) {
 		Index retval = new Index();
 		
 		String[] part1 = line.split("[\\s|\\t]+");
 		if (part1.length == 2)
 		{
 			retval.word = part1[0];
-			String[] part2 = line.split(":");
+			String[] part2 = part1[1].split(":");
 			if (part2.length == 2)
 			{
 				try
@@ -32,5 +37,24 @@ public class IndexParser {
 		}
 		
 		return retval;
+	}
+	
+	public static void main(final String[] args)
+	{
+		String[] lines = {
+				"actor\t1:2.txt<REU001-0020.940701>",
+				"added\t2:1.txt<REU001-0013.940701>",
+				"additional\t2:1.txt<REU001-0013.940701>",
+				"africa\t1:3.txt<REU001-0045.940701>",
+				"after\t1:1.txt<REU001-0013.940701>",
+				"afterwards\t1:3.txt<REU001-0045.940701>"
+		};
+		
+		for (String line : lines)
+		{
+			IndexParser.Index i = IndexParser.parseLine(line);
+			System.out.println(i);
+		}
+		
 	}
 }
